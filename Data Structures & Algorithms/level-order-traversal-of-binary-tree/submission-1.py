65b8ef08e3e1cdcf032e5d836ddbff88967cae67
Amrit Madabushi <76710243+AmritM1015@users.noreply.github.com>
2026-09-07 15:53:37 -0400

    Add: level-order-traversal-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-1.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        return res
+
+        # DFS solution (is not efficient because we repeatedly recurse through the same levels)
+        # We can probably have a inner function which returns the node and depth or adds it to a global dictionary and then create another nested
+        # List storing the different levels
+        # res = [] # res[d] stores all the nodes at depth d
+        # def dfs(node,depth): # Output: int, int
+        #     if not node:
+        #         return
+        #     if len(res) == depth:
+        #         res.append([])
+        #     res[depth].append(node.val)
+        #     dfs(node.left, depth+1)
+        #     dfs(node.right, depth+1)
+        # dfs(root,0)
+        # return res
+
+
