14e3aea347043bfb7a466ef1fe809ccd17e22b95
Amrit Madabushi <76710243+AmritM1015@users.noreply.github.com>
2026-09-07 21:54:42 -0400

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        while len(stones) > 1:
+            stones.sort()
+            x = stones.pop(-1)
+            y = stones.pop(-1)
+            if x != y:
+                stones.append(abs(x-y))
+        return stones[0] if len(stones) > 0 else 0
